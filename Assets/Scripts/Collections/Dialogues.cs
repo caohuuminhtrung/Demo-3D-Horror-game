b96a54c7aef93ae3dfb060ebb8bf8728be3c3a88
Cerberose <tranlethaison123@gmail.com>
2023-11-22 01:31:27 +0700

    fix minor bug and update cutscenes

--- Assets/Scripts/Collections/Dialogues.cs
+++ Assets/Scripts/Collections/Dialogues.cs
@@ -4,7 +4,7 @@ public class Dialogues
     new string[] { "Darling", "Trung", "Have you awoken yet?" , "I brought up the dinner for you", "...", "I'll leave at the door then", "Remember to take the pills" },
     new string[] { "I don't feel like eating right now", "Ugh, those pills again", "It gives me bad sleep, I hate it", "To your usual home, buddy"},
     new string[] { "Feeling a bit sleepy right now", "Guess gotta head to bed then"},
-    new string[] { "Huh?" },
-    new string[] { "What happened here?" },
+    new string[] {},
+    new string[] {},
   };
 }
