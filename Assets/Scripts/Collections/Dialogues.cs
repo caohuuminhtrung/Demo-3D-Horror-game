cb7c1edb1e173e5a36a37068c26d44f9c12b8e56
Flushienime <tomwitwicky303@gmail.com>
2023-11-23 22:58:26 +0700

    chinh dialogue xiu

--- Assets/Scripts/Collections/Dialogues.cs
+++ Assets/Scripts/Collections/Dialogues.cs
@@ -1,8 +1,8 @@
 public class Dialogues
 {
   public string[][] dialogues = new string[][] {
-    new string[] { "Darling?", "", "Are you awake?" , "", "I brought you some dinner.", "", "...", "I'll leave them at the door then.", "Remember to take the pills, ok.", "I gotta go now. See you tommorow." },
-    new string[] { "I'm sorry...", "I don't feel like eating right now.", "", "...those pills again.", "It gives me bad sleep, I hate it.", "", "She'll probably score me again tommorow for throwing them away, but..."},
+    new string[] { "Darling?", "", "Are you awake?" , "", "I brought you some dinner.", "", "...", "I'll leave them at the door then.", "Remember to take the pills, ok.", "I gotta go home now. See you tommorow." },
+    new string[] { "I'm sorry...", "I don't feel like eating right now.", "", "...those pills again.", "They give me bad sleep, I hate it.", "", "She'll probably score me again tommorow for throwing them away, but..."},
     new string[] { "Ugh, my head hurts...", "Guess I'll just go back to sleep."},
     new string[] {"Ughhh, I woke up mid sleep again. Even without those pills..."},
     new string[] {"Huh, what the hell is happening?", "I think I heard something..."},
